a2ddd8d440b375df415d2ebf0ca3bd95f06045e2
Diego <diaamayaram@unal.edu.co>
2021-11-19 15:32:46 +0100

    test_1

--- examples/hal2cff_example.py
+++ examples/hal2cff_example.py
@@ -1,7 +1,26 @@
 from hal2cff import hal2cff
+import ipywidgets as widgets
+from IPython.display import display, display_html, Markdown
 
 # # hal2cff example
 
+
+# +
+query = widgets.Textarea(value='https://hal.archives-ouvertes.fr/hal-01361430v1')
+button = widgets.Button(description="Print CFF")
+display(widgets.HBox([query, button]))
+output = widgets.Output()
+display(output)
+
+def run_and_display_query(_):
+    with output:
+        output.clear_output()
+        #display_html(hal2cff(query.value))
+        display(print(hal2cff(query.value)))
+        
+
+button.on_click(run_and_display_query)
+# -
 
 print(hal2cff("https://hal.archives-ouvertes.fr/hal-01361430v1"))
 
